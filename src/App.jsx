22521aba127ad39dca0c393666d279135336ab5a
JamesFaloonStudent <faloonj@sheridancollege.ca>
2024-12-11 15:16:15 -0500

    htmlSetup

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1> Hello React </h1>
-    </>
+    <div className="calculator">
+      <div className="output">
+        <div className="current-operand"></div>
+        <div className="previous-operand"> </div>
+      </div>
+      <button className="span-two"> AC</button>
+      <button className="span-one">+</button>
+      <button className="span-one">÷</button>
+
+      <button className="span-one">9</button>
+      <button className="span-one">8</button>
+      <button className="span-one">7</button>
+      <button className="span-one">+</button>
+
+      <button className="span-one">6</button>
+      <button className="span-one">5</button>
+      <button className="span-one">4</button>
+      <button className="span-one">-</button>
+
+      <button className="span-one">3</button>
+      <button className="span-one">2</button>
+      <button className="span-one">1</button>
+      <button className="span-one">*</button>
+
+      <button className="span-one">0</button>
+      <button className="span-one">.</button>
+      <button className="span-two">=</button>
+    </div>
   )
 }
 
